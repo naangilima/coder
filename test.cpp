5e9c923f78b1ff020244e8c3d2a7ca35ad3cb4d3
jihyun Ryu <jinnyblossoms@gmail.com>
2015-05-20 22:19:45 +0900

    print test

--- test.cpp
+++ test.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <stdio.h>
 #define MAX 10000
+int map[MAX][MAX];
 
 using namespace std;
 
 void dfs(int (*map)[MAX], int N)
 {
-
+	int i,j;
+	for(i=0;i<N;i++)
+	{
+		for(j=0;j<N;j++)
+		{
+			printf("%d ",map[i][j]);
+		}
+		printf("\n");
+	}
+	
 }
 
 int main()
@@ -15,7 +25,6 @@ int main()
 	int TC,T,N;
 	int i,j;
 
-
 	scanf("%d",&TC);
 	while(TC--)
 	{
@@ -23,11 +32,13 @@ int main()
 
 		for(i=0;i<N;i++)
 		{
-
-		}
-
-		printf("test");
-	    	
+			for(j=0;j<N;j++)
+			{
+				scanf("%d",&map[i][j]);
+			}
+		}	
+	
+		dfs(map,N);
 	}
 
 
