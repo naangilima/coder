fbc8273fe80c0e09850da5f6286e165cd37ad86e
jihyun Ryu <jinnyblossoms@gmail.com>
2015-05-20 06:07:02 -0700

    commit practice version

---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+#define MAX 10000
+
+using namespace std;
+
+void dfs(int (*map)[MAX], int N)
+{
+
+}
+
+int main()
+{
+
+	int TC,T,N;
+	int i,j;
+
+
+	scanf("%d",&TC);
+	while(TC--)
+	{
+		scanf("%d",&N);
+
+		for(i=0;i<N;i++)
+		{
+
+		}
+
+		printf("test");
+	    	
+	}
+
+
+	return 0;
+}
